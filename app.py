e9b04d2088f19a78037ab510fed675a186ffd6a3
mostanglan <xsonxhrestha2016@gmail.com>
2023-06-14 19:02:39 +0545

    form request handle

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify, make_response
+from flask import Flask, jsonify, make_response, request
 import mysql.connector
 import cosine_similarity
 
@@ -14,43 +14,49 @@ except mysql.connector.Error as err:
 	print("[-]Database Error :: {}".format(err))
 
 
-
-mycursor = mydb.cursor()
-
-mycursor.execute("SELECT * FROM user_tb")
-
-myresult = mycursor.fetchall()
-
 app = Flask(__name__)
 
-@app.route('/cosine')
+@app.route('/cosine', methods = ['POST', 'GET'])
 def getData():
+	try:
+		user_gender = request.form['gender']
+		"male"
+		my_cursor = mydb.cursor(dictionary=True)
+		my_cursor.execute("SELECT userid, lastname, age, height, gender, bio FROM user_tb WHERE gender = %s",['Male' if user_gender == 'Female' else 'Female'])
+		my_partner = my_cursor.fetchall()
 
-	person1 = {
-        'lastname': 'smith',
-        'age': 30,
-        'height': 180,
-        'gender': 'male',
-        'bio': 'i am a software engineer with a passion for coding'
-    }
-	person2 = {
-        'lastname': 'smith',
-        'age': 35,
-        'height': 175,
-        'gender': 'female',
-        'bio': 'i work as a date scientist and enjoy analyzing complex data'
-    }
+		person1 = {
+			'lastname': request.form['lastname'],
+			'age': int(request.form['age']),
+			'height': int(request.form['height']),
+			'gender': user_gender,
+			'bio': request.form['bio']
+    	}
+		results = [{'status': 'success'}]
+		if my_partner:
+			for row in my_partner:
+				person2 = {
+					'lastname': row['lastname'],
+					'age': row['age'],
+					'height': row['height'],
+					'gender': row['gender'],
+					'bio': row['bio']
+				}
+				cosine_result = cosine_similarity.calculateCosineSimilarity(person1, person2)
+				results.append({'id': row['userid'], 'cosine': cosine_result})
+				
+			response = make_response(
+				jsonify(results),
+				200
+			)
+			return response
+		else: 
+			return make_response(jsonify({"status": "failed", "message": "no data found."}), 400) 
+	except mysql.connector.Error as err:
+		return make_response(jsonify({"status": "failed", "message": f"Something went wrong :: {err}."}), 400) 
+	finally:
+		my_cursor.close()
 
-	cosine_result = cosine_similarity.calculateCosineSimilarity(person1, person2)
-	
-	response = make_response(
-		jsonify(
-			{"cosine_similarity": str(cosine_result)}
-		),
-		200
-	)
-	
-	return response
 
 if __name__ == "__main__":
 	app.run(debug=True)
